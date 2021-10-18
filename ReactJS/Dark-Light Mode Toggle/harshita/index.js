b6395845ef9269e8c86010fcaf44a96f935a7358
harshitasrivasatva <harshitasriavastava376@gmail.com>
2021-10-18 19:06:08 +0530

    adding dark-light mode toggle

---
+++ ReactJS/Dark-Light Mode Toggle/harshita/index.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import data from './data'
+import Article from './Article'
+
+const getStorage = () => {
+  let theme = "light-theme"
+
+  if(localStorage.getItem('theme')){
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+}
+
+function App() {
+  let [theme,setTheme] = useState(getStorage())
+
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme);
+  },[theme]);
+
+  const changeTheme = () => {
+    {
+      theme=="light-theme" ? setTheme("dark-theme") : setTheme("light-theme")
+    }
+
+    // if(theme=="light-theme"){
+    //   setTheme("dark-theme")
+    // } else { 
+    //   setTheme("light-theme")
+    // }
+  }
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={changeTheme}>Toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {
+          data.map((item) => {
+            return <Article key={item.id} {...item} />
+          })
+        }
+      </section>
+    </main>
+  )
+}
+
+export default App
